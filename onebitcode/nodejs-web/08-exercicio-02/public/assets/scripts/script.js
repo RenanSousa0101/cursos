fe1802db692f83a7a4bd2dfe24aa8e97f1fe845c
RenanSousa <renansousa26ti@gmail.com>
2025-01-06 11:21:07 -0300

    atualiza nodejs-web

---
+++ onebitcode/nodejs-web/08-exercicio-02/public/assets/scripts/script.js
@@ -0,0 +1,11 @@
+// const taskModel = require('../../../src/models/taskModel');
+// const listTask = taskModel.getAllTasks();
+
+// listTask.forEach(task => {
+//     task.tasks.forEach(tasks => {
+//         if(tasks.check === true){
+//             let span = document.getElementById(tasks.id);
+//             span.classList.add('check');
+//         }
+//     })
+// })
