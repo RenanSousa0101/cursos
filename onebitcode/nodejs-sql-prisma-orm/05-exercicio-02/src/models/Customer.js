fcd1e64fed517ad62e3842959df88c16ebb6b2c5
RenanSousa <renansousa26ti@gmail.com>
2025-03-06 17:45:03 -0300

    atualiza nodejs-sql-prisma-orm

--- onebitcode/nodejs-sql-prisma-orm/05-exercicio-02/src/models/Customer.js
+++ onebitcode/nodejs-sql-prisma-orm/05-exercicio-02/src/models/Customer.js
@@ -13,4 +13,52 @@ class Customer {
         const result = await query(`SELECT * FROM customers;`)
         return result.rows.map((row) => new Customer(row))
     }
+
+    static async create({ name, email }) {
+       const result = await query(
+            `INSERT INTO customers (name, email)
+            VALUES ($1, $2)
+            RETURNING *`,
+            [name, email]
+        )
+        return new Customer(result.rows[0])
+    }
+
+    static async findById(id) {
+        const result = await query(`SELECT * FROM customers WHERE id = $1`, [id])
+        if (!result.rows[0]) return null
+        return new Customer(result.rows[0])
+    }
+
+    static async update(id, attributes) {
+        const { rows } = await query(`SELECT * FROM customers WHERE id = $1`, [id])
+        if (!rows[0]) return null
+      
+        const customer = new Customer(rows[0])
+    
+        Object.assign(customer, attributes)
+        customer.updatedAt = new Date()
+    
+        await query(
+          `UPDATE customers SET
+            name = $1,
+            email = $2,
+            updated_at = CURRENT_TIMESTAMP
+          WHERE id = $3;`,
+          [
+            customer.name,
+            customer.email,
+            customer.id
+          ]
+        )
+    
+        return customer
+    }
+
+    static async delete(id) {
+        await query(`DELETE FROM customers WHERE id = $1`, [id])
+        return { message: "Customer deleted successfully." }
+    }
 }
+
+module.exports = Customer
--- onebitcode/nodejs-sql-prisma-orm/05-exercicio-02/src/router.js
+++ onebitcode/nodejs-sql-prisma-orm/05-exercicio-02/src/router.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
-
+const customerControler = require("./controllers/customers-controller")
 
 const router = Router();
 
+router.get("/customers", customerControler.index)
+router.post("/customers", customerControler.create)
+router.get("/customers/:id", customerControler.show)
+router.put("/customers/:id", customerControler.update)
+router.delete("/customers/:id", customerControler.delete)
 
 
 module.exports = router;
