c62f2fd387bc1fb08e86f43d612f3a0e23046666
RenanSousa <renansousa26ti@gmail.com>
2025-03-15 16:49:05 -0300

    atualiza nodejs-sql-prisma-orm

---
+++ onebitcode/nodejs-sql-prisma-orm/10-exercicio-03/src/routes/posts.js
@@ -0,0 +1,49 @@
+const { Router } = require("express");
+const prisma = require("../database");
+
+const router = Router()
+
+router.get("/", async (req, res) => {
+    const posts = await prisma.post.findMany({
+        orderBy: { createdAt: "desc" }
+    })
+    res.json(posts)
+})
+
+router.post("/", async (req, res) => {
+    const newPost = await prisma.post.create({
+        data: {
+            title: req.body.title,
+            slug: req.body.slug,
+            content: req.body.content,
+            published: req.body.published,
+            authorId: req.body.authorId
+        }
+    })
+    res.status(201).json(newPost)
+})
+
+router.get("/:id", async (req, res) => {
+    const post = await prisma.post.findUnique({
+        where: { id: Number(req.params.id) },
+        include: { author: true }
+    })
+    res.json(post)
+})
+
+router.put("/:id", async (req, res) => {
+    const updatedPost =await prisma.post.update({
+        data: req.body,
+        where: { id: Number(req.params.id )}
+    })
+    res.json(updatedPost)
+})
+
+router.delete("/:id", async (req, res) => {
+    const deletedPost = await prisma.post.delete({
+        where: { id: Number(req.params.id) }
+    })
+    res.json({ deletedPost })
+})
+
+module.exports = router;
