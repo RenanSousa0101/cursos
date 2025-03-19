861f144c909e42d8e3680efe326797947e9f0263
RenanSousa <renansousa26ti@gmail.com>
2025-03-19 16:56:55 -0300

    atualiza nodejs-sql-prisma-orm

--- onebitcode/nodejs-sql-prisma-orm/14-projeto-final/src/controllers/LeadsController.ts
+++ onebitcode/nodejs-sql-prisma-orm/14-projeto-final/src/controllers/LeadsController.ts
@@ -1,14 +1,42 @@
 import { Handler } from "express";
 import { prisma } from "../database";
-import { CreateLeadRequestSchema, updateLeadRequestSchema } from "./schemas/LeadsRequestSchemas";
+import { CreateLeadRequestSchema, GetLeadsRequestSchema, updateLeadRequestSchema } from "./schemas/LeadsRequestSchemas";
 import { HttpError } from "../errors/HttpError";
 import { nextTick } from "process";
+import { Prisma } from "@prisma/client";
 
 export class LeadsController {
     index: Handler = async (req, res, next) => {
         try {
-            const leads = await prisma.lead.findMany()
-            res.json(leads)
+            const query = GetLeadsRequestSchema.parse(req.query)
+            const { page = "1", pageSize = "10", name, status, sortBy = "name", order = "asc" } = query
+
+            const pageNumber = Number(page)
+            const pageSizeNumber = Number(pageSize)
+
+            const where: Prisma.LeadWhereInput = {}
+
+            if(name) where.name = { contains: name, mode: "insensitive" }
+            if(status) where.status = status
+
+            const leads = await prisma.lead.findMany({
+                where,
+                skip: (pageNumber - 1) * pageSizeNumber,
+                take: pageSizeNumber,
+                orderBy: { [sortBy]: order }
+            })
+
+            const total = await prisma.lead.count({ where })
+
+            res.json({
+                data: leads,
+                meta: {
+                    page: pageNumber,
+                    pageSize: pageSizeNumber,
+                    total,
+                    totalPages: Math.ceil(total / pageSizeNumber)
+                }
+            })
         } catch (error) {
             next(error)
         }
