05d44b7d46e362daf6836492fa147f58b3d1a710
RenanSousa <renansousa26ti@gmail.com>
2025-03-18 21:10:09 -0300

    atualiza nodejs-sql-prisma-orm

--- onebitcode/nodejs-sql-prisma-orm/14-projeto-final/src/router.ts
+++ onebitcode/nodejs-sql-prisma-orm/14-projeto-final/src/router.ts
@@ -1,6 +1,14 @@
 import { Router } from "express"
+import { LeadsController } from "./controllers/LeadsController"
+
 
 const router = Router()
+
+const leadsController = new LeadsController()
+
+router.get("/leads", leadsController.index)
+router.post("/leads", leadsController.create)
+router.get("leads/:id", leadsController.show)
 
 router.get("/status", async (req, res, next) => {
     try {
