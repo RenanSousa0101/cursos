40a5a7121dbe2b54e3f364ec305b198c4dee8d48
Renan Sousa <renansousa26ti@gmail.com>
2025-04-04 10:32:18 -0300

    atualização React

---
+++ onebitcode/react/17-state/src/components/Profile/index.jsx
@@ -0,0 +1,44 @@
+import Title from "../Title"
+import ProfileSection from "./ProfileSection"
+import styles from "./styles.module.css"
+import LinkButton from "../LinkButton"
+import { useState } from "react"
+
+export default function Profile(props) {
+
+    const [followText, setFollowText] = useState("Follow")
+
+    function handleClick(ev) {
+        alert("Você agora está seguindo!")
+        setFollowText("Following")
+    }
+
+    return (
+        <div className={styles.container}>
+            <img className={styles.avatar} src={props.avatar} alt={props.name} />
+            <Title>
+                <span>Name: </span>
+                {props.name}
+                <button
+                    className={styles.followButton}
+                    onClick={handleClick}
+                >
+                    {followText}
+                </button>
+            </Title>
+            <ProfileSection>{props.bio}</ProfileSection>
+            <ProfileSection>{props.phone}</ProfileSection>
+            <ProfileSection>{props.email}</ProfileSection>
+            <ProfileSection
+                className={styles.links}
+                id="links-section"
+                data-test="some value"
+                aria-label="social links"
+            >
+                <LinkButton href={props.githubUrl}>GitHub</LinkButton>
+                <LinkButton href={props.linkedinUrl}>LinkedIn</LinkButton>
+                <LinkButton href={props.twitterUrl}>Twitter</LinkButton>
+            </ProfileSection>
+        </div>
+    )
+}
