7d59b85f5708cc2388e714c804d0d14d992770ee
Renan Sousa <renansousa26ti@gmail.com>
2025-04-04 10:22:30 -0300

    atualização React

---
+++ onebitcode/react/16-eventos-js-react/src/components/Profile/index.jsx
@@ -0,0 +1,40 @@
+import Title from "../Title"
+import ProfileSection from "./ProfileSection"
+import styles from "./styles.module.css"
+import LinkButton from "../LinkButton"
+
+function handleClick(ev) {
+    console.log(ev)
+    alert("Você agora está seguindo!")
+}
+
+export default function Profile(props) {
+    return (
+        <div className={styles.container}>
+            <img className={styles.avatar} src={props.avatar} alt={props.name} />
+            <Title>
+                <span>Name: </span>
+                {props.name}
+                <button
+                    className={styles.followButton}
+                    onClick={handleClick}
+                >
+                    Follow
+                </button>
+            </Title>
+            <ProfileSection>{props.bio}</ProfileSection>
+            <ProfileSection>{props.phone}</ProfileSection>
+            <ProfileSection>{props.email}</ProfileSection>
+            <ProfileSection
+                className={styles.links}
+                id="links-section"
+                data-test="some value"
+                aria-label="social links"
+            >
+                <LinkButton href={props.githubUrl}>GitHub</LinkButton>
+                <LinkButton href={props.linkedinUrl}>LinkedIn</LinkButton>
+                <LinkButton href={props.twitterUrl}>Twitter</LinkButton>
+            </ProfileSection>
+        </div>
+    )
+}
