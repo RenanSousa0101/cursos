744a227b2c5e1b7a36287613f79617a1dd8b7714
Renan Sousa <renansousa26ti@gmail.com>
2025-04-04 13:43:12 -0300

    atualização React

---
+++ onebitcode/react/23-renderizacao-condicional/src/components/Input.jsx
@@ -0,0 +1,24 @@
+/* 
+Se quisermos ainda podemos utilizar o prop-types para validação, como o ESLint recomenda. 
+Para isso, instale a biblioteca:
+npm install prop-types
+*/
+
+import PropTypes from "prop-types"
+
+Input.propTypes = {
+    passwordSize: PropTypes.number.isRequired,
+    setPasswordSize: PropTypes.func.isre
+}
+
+export default function Input(props) {
+    return (
+        <input
+            type="number"
+            id="passwordSize"
+            min={1}
+            value={props.passwordSize}
+            onChange={(ev) => props.setPasswordSize(ev.target.value)}
+        />
+    )
+}
