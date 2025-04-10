be850f410859bea477c79280d8d2ecd26bb5f1cb
Renan Sousa <renansousa26ti@gmail.com>
2025-04-10 18:54:11 -0300

    atualização Next.js

---
+++ onebitcode/next/08-layouts-vs-templates/src/app/(site)/blog/layout.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { ReactNode, useEffect, useState } from "react";
+import styles from "./layout.module.css"
+import Link from "next/link"
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log("Render")
+    }, []);
+
+    return (
+        <div className={styles.blogLayout}>
+            <aside>
+                <h2>Counter: {counter}</h2>
+                <button onClick={() => setCounter(current => current + 1)}>
+                    Incrementar
+                </button>
+                <div>
+                    <h2>Categorias</h2>
+                    <ul className="cards">
+                        <li className="card">
+                            <Link href="/blog/categorias/planetas">Planetas</Link>
+                        </li>
+                        <li className="card">
+                            <Link href="/blog/categorias/estrelas">Estrelas</Link>
+                        </li>
+                        <li className="card">
+                            <Link href="/blog/categorias/galaxias">Galáxias</Link>
+                        </li>
+                        <li className="card">
+                            <Link href="/blog/categorias/cosmologia">Cosmologia</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2>Galerias</h2>
+                    <div>
+                        <p>Explore imagens incríveis do universo.</p>
+                        <ul className="cards">
+                            <li className="card">
+                                <a href="#">Imagem de uma Estrela Distante</a>
+                            </li>
+                            <li className="card">
+                                <a href="#">Nebulosa em Explosão</a>
+                            </li>
+                            <li className="card">
+                                <a href="#">A Grande Galáxia de Andrômeda</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </aside>
+            <div>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+/* 
+Ao renomearmos o arquivo layout.tsx para template.tsx podemos perceber que o visual continua o mesmo 
+entre todas as páginas, mas a cada nova página ele zera o contador e exibe a mensagem "Render" 
+novamente no console.
+
+*/
