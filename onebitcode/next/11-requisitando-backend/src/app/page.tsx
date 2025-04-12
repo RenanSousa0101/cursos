7188d3acd6cf8c5a6a4a3458a9e355ea0d8baa48
Renan Sousa <renansousa26ti@gmail.com>
2025-04-12 10:01:06 -0300

    atualização Next.js

---
+++ onebitcode/next/11-requisitando-backend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { PlanetsManager } from '@/components/PlanetsManager';
+import styles from './page.module.css';
+
+export default async function Page() {
+    const response = await fetch('http://localhost:3000/api/planets');
+    const planets = await response.json();
+
+    return (
+        <div className={styles.page}>
+            <h1>Planetas</h1>
+            <PlanetsManager startingPlanets={planets} />
+        </div>
+    );
+};
