d9e3e6776a7b02842dd59802b96efd4427328b30
Renan Sousa <renansousa26ti@gmail.com>
2025-04-12 15:19:20 -0300

    atualização Next.js

---
+++ onebitcode/next/15-tratamento-erros/src/app/error.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+interface ErrorProps {
+    error: Error
+    reset: () => void
+}
+
+export default function Error({ error, reset}: ErrorProps) {
+    return (
+        <main>
+            <h2>Ocorreu um erro!</h2>
+            <p>{error.message}</p>
+            <button 
+                type="button"
+                onClick={() => reset()}
+            >
+                Tentar Novamente
+            </button>
+        </main>
+    )
+}
---
+++ onebitcode/next/15-tratamento-erros/src/models/Planet.ts
@@ -0,0 +1,40 @@
+import { pool } from "@/database/pool";
+import { resolve } from "path";
+
+export interface Planet {
+    id: number
+    name: string
+    description: string
+    imageUrl: string
+}
+
+interface CreatePlanetParams {
+    name: string,
+    description: string,
+    imageUrl: string
+}
+
+function wait(ms: number) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(true), ms);
+    })
+}
+
+export async function getPlanets() {
+    await wait(4000);
+    throw new Error('Erro ao consultar o banco de dados!');
+    const result = await pool.query('SELECT id, name, description, image_url AS "imageUrl" FROM planets;');
+    const planets: Planet[] = result.rows;
+    return planets;
+}
+
+export async function createPlanet(params: CreatePlanetParams) {
+    await pool.query(
+        'INSERT INTO planets (name, description, image_url) VALUES ($1, $2, $3)',
+        [params.name, params.description, params.imageUrl]
+    );
+}
+
+export async function deletePlanet(id: number) {
+    await pool.query('DELETE FROM planets WHERE id = $1', [id]);
+}
